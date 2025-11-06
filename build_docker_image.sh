d6f7f1fd03a7cdd776949a41bdc437e52b66e054
ryuinw123 <47142982+ryuinw123@users.noreply.github.com>
2025-11-06 23:09:15 +0700

    Update build_docker_image.sh

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
 image_name="open-webui"
+ghcr_repo="ghcr.io/courseville/${image_name}"
+#github_pat="ghp_pnbMhNI7PXOtnnPvB7Pagp8NsK22z72Sxxxx"
+github_pat=""
+#github_username="ryuinxxxx"
+github_username=""
+
+# Authenticate to GHCR
+echo ${github_pat} | docker login ghcr.io -u ${github_username} --password-stdin
+
+# Create a builder if it doesn't exist
+docker buildx create --use --name multiarch-builder || docker buildx use multiarch-builder
+
+# Build multi-platform image
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t "$ghcr_repo" \
+  --push .  # Important: use --push when using buildx for remote registry
